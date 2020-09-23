7623de7715a4550ea2d3706330426c4445b2968d
yingyang <393050600@qq.com>
2020-09-23 19:24:14 +0800

    小顶堆排序

---
+++ src/main/java/com/sort/mergesort/MergeSort.java
@@ -0,0 +1,54 @@
+package com.sort.mergesort;
+
+/**
+ * Created by yang on 2020/9/23.
+ */
+public class MergeSort {
+
+    public static void main(String[] args) {
+        int[] array = { 49, 38, 65, 97, 76, 13, 27, 50 };
+        mergeSort(array, 0, array.length - 1);
+        System.out.println("排好序的数组：");
+        for (int e : array) {
+            System.out.println(e + " ");
+        }
+    }
+
+    private static void mergeSort(int[] a, int low, int high) {
+        int mid = (low + high) / 2;
+        if (low < high) {
+            mergeSort(a, low, mid);
+            mergeSort(a, mid + 1, high);
+            //左右归并
+            merge(a, low, mid, high);
+        }
+    }
+
+    private static void merge(int[] a, int low, int mid, int high) {
+        int[] temp = new int[high - low + 1];
+        int i = low;
+        int j = mid + 1;
+        int k = 0;
+        // 把较小的数先移到新数组中
+        while (i <= mid && j <= high) {
+            if (a[i] < a[j]) {
+                temp[k++] = a[i++];
+            }
+            else {
+                temp[k++] = a[j++];
+            }
+        }
+        // 把左边剩余的数移入数组
+        while (i <= mid) {
+            temp[k++] = a[i++];
+        }
+        // 把右边边剩余的数移入数组
+        while (j <= high) {
+            temp[k++] = a[j++];
+        }
+        // 把新数组中的数覆盖nums数组
+        for (int x = 0; x < temp.length; x++) {
+            a[x + low] = temp[x];
+        }
+    }
+}

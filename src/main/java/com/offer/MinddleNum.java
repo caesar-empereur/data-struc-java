1d2b9ed0cdfcaf425a0055c96bf9bcb1ec7864db
yingyang <393050600@qq.com>
2020-10-12 17:25:22 +0800

    大小顶堆找中位数

--- src/main/java/com/offer/MinddleNum.java
+++ src/main/java/com/offer/MinddleNum.java
@@ -17,6 +17,21 @@ public class MinddleNum {
         System.out.println(findMinddleNum(array));
     }
 
+    /**
+     * 不用排序的方式，而是用大顶堆，小顶堆的思路来实现
+     * 不断的遍历数组，把数组的数拿出来判断，放到2个堆里面，放完之后调整堆
+     *
+     * 大顶堆，用来存放数组较小的数，放在左边
+     * 小顶堆，用来存放数组较大的数，放在右边
+     *
+     * 如果数组长度是奇数，左边的大顶堆中存放 n/2+1 个数，右边的小顶堆存放剩下的数
+     * 数组遍历完之后，左边的大顶堆的堆顶就是中位数
+     *
+     * 如果数组长度是奇数，左边的大顶堆中存放 n/2 个数，右边的小顶堆存放n/2 个数
+     * 数组遍历完之后，大顶堆和小顶堆的堆顶的平均数就是 中位数
+     * @param array
+     * @return
+     */
     private static double findMinddleNum(int[] array) {
         //小顶堆，用来存放数组较大的数，放在右边
         PriorityQueue<Integer> minHeap = new PriorityQueue<>();
